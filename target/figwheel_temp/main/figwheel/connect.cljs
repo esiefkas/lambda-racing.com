7764357d535df83a2f27eb3b6747175ce1dd6271
Eric Siefkas <eric@thinktopic.com>
2016-10-14 20:51:04 -0600

    forgot to push a jar /barf

--- target/figwheel_temp/main/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [figwheel.client] [lambda-racing.core] [figwheel.client.utils]))
-(figwheel.client/start {:build-id "main", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
